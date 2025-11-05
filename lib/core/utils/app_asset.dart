cb6109115de1a716f32a410cee0e930d9efc7efa
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-11-05 16:25:07 +0300

    addint the important packages like localization and image helper and also the icon luanch icon

---
+++ lib/core/utils/app_asset.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: prefer_single_quotes
+class AppAsset {
+  AppAsset._();
+  
+  static const String imagesLogo = "assets/images/logo.png";
+}
+
--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,8 +1,8 @@
   import 'package:flutter/material.dart';
 
   abstract class AppColors {
-    static const Color primaryColor = Color(0xFFC3322B);
-    static const Color secondaryColor = Color(0xff064061);
+    static const Color primaryColor = Color(0xFF802A33);
+    static const Color secondaryColor = Color(0xff411B2A);
     static const Color errorColor = Color(0xffF3735E);
-    static const Color successColor = Color(0xff7DD97B);
+    static const Color successColor = Color(0xff009926);
   }
