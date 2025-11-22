da0302b8dd0d99063a4597b49580cf267fb1e6fb
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-11-23 00:48:08 +0300

    integrate supabase with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:dinereserve/core/router/app_router.dart';
 import 'package:dinereserve/core/theme/light_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://aqvagkphjqcdqnrpvebu.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxdmFna3BoanFjZHFucnB2ZWJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4NDMzMjAsImV4cCI6MjA3OTQxOTMyMH0.coGyz5okv_PbJH01lDSb10tqcRbUKz_wYeNas_M9eCI',
+  );
+
   runApp(const Dinereserve());
 }
 
